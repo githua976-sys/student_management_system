2af7611b4a6b1062323328388fc8a3bf376f2ee7
githua976sys <githua976@gmail.com>
2026-06-23 11:01:29 +0300

    update database.py

--- database.py
+++ database.py
@@ -4,12 +4,21 @@ def create_database():
     conn = sqlite3.connect("students.db")
     cursor = conn.cursor()
 
+    # Students Table
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS students (
         student_id TEXT PRIMARY KEY,
-        name TEXT,
+        name TEXT NOT NULL,
         age INTEGER,
         email TEXT
+    )
+    """)
+
+    # Courses Table
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS courses (
+        course_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        course_name TEXT NOT NULL
     )
     """)
 
